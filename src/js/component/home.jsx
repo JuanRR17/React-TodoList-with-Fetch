d77242f9fcd25b50304387f66a1825421f4f20db
JuanRR17 <juanrepeto@gmail.com>
2022-09-01 18:00:43 +0200

    todo list

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,42 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useState } from "react";
+import ToDo from "./ToDo.jsx";
 
 //create your first component
 const Home = () => {
+	const [toDoList, setToDoList]=useState([]);
+	const [value, setValue]=useState("");
+	const [id, setId] = useState("1");
+
+	const handleKeyDown = (e) =>{
+		if(e.key === 'Enter'){
+			let item = {
+				id: id,
+				string: e.target.value
+			}
+			setToDoList([...toDoList,item])
+			setValue("")
+			setId(id => id + 1)
+		}
+	}
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+			<div className="w-50 m-auto">
+				<h1 className="text-center">todos</h1>
+				<ul className="list-group">
+					<input className="list-group-item" 
+						onKeyDown={handleKeyDown}
+						onChange={e => setValue(e.target.value)}
+						value={value}
+					/>
+					{toDoList.map((toDo) => {
+						return <ToDo 
+						toDo={toDo} 
+						toDoList={toDoList}
+						key={toDo.id} 
+						handleSetToDoList={value=>setToDoList(value)}/>
+						})}
+				</ul>
+			</div>
 	);
 };
 
